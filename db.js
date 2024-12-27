0f7a608be3ddf84505f3fdccdd4a5625e7d79af7
venom <113086378+officialbhaveshmeena@users.noreply.github.com>
2024-12-27 17:25:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const { Pool } = require("pg");
+const dotenv = require("dotenv");
 
+// dot env config
+dotenv.config();
 const pool = new Pool({
-  user: "bhavesh", // Replace with your PostgreSQL username
-  host: "dpg-ctn8pq3v2p9s73fhljtg-a",
-  database: "testdb_9xgp",
-  password: "MNv7jRj8LIcckFXmcd3vshZMONviJPKD", // Replace with your PostgreSQL password
+  user: process.env.user, // Replace with your PostgreSQL username bhavesh
+  host: process.env.host,
+  database: process.env.database,
+  password: process.env.password, // Replace with your PostgreSQL password
   port: 5432,
 });
 
